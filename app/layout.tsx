da9c6a32a12385cf12490be9f7657001fd05dcab
SAFE <safeprod@protonmail.com>
2025-08-29 18:17:01 +0530

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,26 +1,26 @@
-import type { Metadata } from "next";
-import "./globals.css";
+import "./globals.css"
+import type { Metadata } from "next"
 import localFont from 'next/font/local'
 
 export const metadata: Metadata = {
   title: "SAFE",
-  description: "Superintelligence Alignment through Financial Engineering",
-};
+  description: "Superintelligence Alignment through Financial Engineering.",
+}
  
-const productFont = localFont({
+const safeFont = localFont({
   src: [
-    {path: '../public/fonts/CMUSerif-Roman.woff2', weight: '400'},
-    {path: '../public/fonts/CMUSerif-Bold.woff2', weight: '700'}
+    {path: '../public/fonts/safeFontRegular.woff2', weight: '400'},
+    {path: '../public/fonts/safeFontBold.woff2', weight: '700'}
   ]
 })
  
-export default function Product({
+export default function SAFE({
   children,
 }: {
   children: React.ReactNode
 }) {
   return (
-    <html lang="en" className={productFont.className}>
+    <html lang="en" className={safeFont.className}>
       <body>{children}</body>
     </html>
   )
