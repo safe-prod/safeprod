a6121775e1307e4b3024a4cd88669e6bd39a11e8
SAFE <safeprod@protonmail.com>
2025-08-30 13:18:10 +0530

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,22 +1,22 @@
-import './globals.css'
-import type { Metadata } from 'next'
-import localFont from 'next/font/local'
+import "./globals.css"
+import type { Metadata } from "next" 
+import localFont from "next/font/local"
 
 export const metadata: Metadata = {
-  title: 'SAFE',
-  description: 'Superintelligence Alignment through Financial Engineering.',
+  title: "SAFE",
+  description: "Superintelligence Alignment through Financial Engineering.",
 }
  
 const safeFont = localFont({
   src: [
-    {path: '../public/fonts/safeFontRegular.woff2', weight: '400'},
-    {path: '../public/fonts/safeFontBold.woff2', weight: '700'}
+    {path: "../public/fonts/safeFontRegular.woff2", weight: "400"},
+    {path: "../public/fonts/safeFontBold.woff2", weight: "700"}
   ]
 })
  
 export default function SAFE({children}: {children: React.ReactNode}) {
   return (
-    <html lang='en' className={safeFont.className}>
+    <html lang="en" className={safeFont.className}>
       <body>{children}</body>
     </html>
   )
