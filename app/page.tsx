d67788d962e139f19a5764df26cae86d60957352
SAFE <safeprod@protonmail.com>
2025-08-30 13:08:24 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,13 @@
+import { SI } from './si.ts'
+
 export default function Portfolio() {
+  text = SI('openai/gpt-oss-20b', 'Hello')
+  alert(text)
   return (
     <div>
       <b>SAFE</b>
       <br/>
       Superintelligence Alignment through Financial Engineering
     </div>
-  );
+  )
 }
