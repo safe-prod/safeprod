a3001c6a6e9728263a16bfad823ec7c6c0bd1977
SAFE <safeprod@protonmail.com>
2025-08-30 13:01:19 +0530

    Create si.ts

---
+++ app/si.ts
@@ -0,0 +1,17 @@
+import { createOpenRouter } from '@openrouter/ai-sdk-provider'
+import { streamText } from 'ai'
+import { z } from '@openrouter-monorepo/type-utils/zod'
+
+export const SI = async (modelName: string, prompt: string) => {
+  const openrouter = createOpenRouter({
+    apiKey: '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'
+  })
+
+  const response = streamText({
+    model: openrouter(modelName),
+    prompt: prompt
+  })
+
+  await response.consumeStream()
+  return response.text
+}
