a0b47fff60c605bde677b1b184e77751d207dd17
SAFE <safeprod@protonmail.com>
2025-08-30 13:33:57 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -2,12 +2,14 @@ import { SI } from "./si.ts"
 
 export default function Portfolio() {
   let testResponse = SI("openai/gpt-oss-20b", "Hello")
-  alert(testResponse)
+  
   return (
     <div>
       <b>SAFE</b>
       <br/>
       Superintelligence Alignment through Financial Engineering
+      <br/>
+      {testResponse}
     </div>
   )
 }
