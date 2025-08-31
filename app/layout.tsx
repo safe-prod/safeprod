e14c0f38bb0619ce2b2bc447befb402402c694fc
SAFE <safeprod@protonmail.com>
2025-08-31 18:13:57 +0530

    Update and rename _app.tsx to layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,6 @@
 import { AppProps } from "next/app"
 import Head from "next/head"
-import "../private/globals.css"
+import "./globals.css"
 import localFont from "next/font/local"
  
 const safeFont = localFont({
