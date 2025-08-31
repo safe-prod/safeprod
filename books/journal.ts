02b8e10af8529bb3ef9710705ef77c9df73e5135
SAFE <safeprod@protonmail.com>
2025-08-31 13:17:31 +0530

    Create books folder

--- books/journal.ts
+++ books/journal.ts
@@ -2,7 +2,7 @@ import { streamText } from "ai"
 import { createOpenRouter } from "@openrouter/ai-sdk-provider"
 import { z } from "zod"
 
-export const SI = async (modelName: string, prompt: string) => {
+export const Journal = async (modelName: string, prompt: string) => {
   const openrouter = createOpenRouter({
     apiKey: "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"
   })
