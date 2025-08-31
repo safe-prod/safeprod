6cb79ebc3cd4be7c8f910888a229acdae32c987e
SAFE <safeprod@protonmail.com>
2025-08-31 14:27:23 +0530

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,6 @@
 import { AppProps } from "next/app"
 import Head from "next/head"
-import "../styles/globals.css"
+import "../utils/globals.css"
 import localFont from "next/font/local"
  
 const safeFont = localFont({
