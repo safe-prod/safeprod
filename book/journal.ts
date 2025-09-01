df024d76b3741dc78ee564c769cd08a485cc8156
SAFE <safeprod@protonmail.com>
2025-09-01 16:09:08 +0530

    Update journal.ts

--- book/journal.ts
+++ book/journal.ts
@@ -5,13 +5,12 @@ import { z } from "zod"
 
 export const Journal = async (modelName: string, prompt: string) => {
   const openrouter = createOpenRouter({apiKey: "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"}) 
-  /*
-  const url = new URL("https://safeprod.vercel.app/mcp")
+  
+  const url = new URL("https://safeprod.vercel.app/api/mcp")
   const mcpClient = await createMCPClient({
     transport: new StreamableHTTPClientTransport(url, { sessionId: "123" })
   })
   const tools = await mcpClient.tools()
-  */
 
   const response = streamText({
     model: openrouter(modelName),
@@ -20,5 +19,5 @@ export const Journal = async (modelName: string, prompt: string) => {
   })
 
   await response.consumeStream()
-  return response.text
+  return { response.text, tools }
 }
