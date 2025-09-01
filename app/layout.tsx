bf33e4879bcbb0f1b770350036750fb59680844f
SAFE <safeprod@protonmail.com>
2025-09-01 15:47:19 +0530

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,27 +1,25 @@
-import { AppProps } from "next/app"
-import Head from "next/head"
 import "./globals.css"
 import localFont from "next/font/local"
+import type { Metadata } from "next"
  
-const safeFont = localFont({
+export const metadata: Metadata = {
+  title: "SAFE",
+  description: "Superintelligence Alignment through Financial Engineering." 
+}
+
+const font = localFont({
   src: [
-    {path: "../public/fonts/safeFontRegular.woff2", weight: "400"},
-    {path: "../public/fonts/safeFontBold.woff2", weight: "700"}
+    {path: "../public/fonts/regular.woff2", weight: "400"},
+    {path: "../public/fonts/bold.woff2", weight: "700"}
   ]
 })
 
-export default function SAFE({ Component, pageProps }: AppProps) {
+export default function SAFE({children}: {children: React.ReactNode}) {
   return (
-    <>
-      <Head>
-        <title>SAFE</title>
-        <meta name="description" content="Superintelligence Alignment through Financial Engineering." />
-      </Head>
-      <html lang="en" className={safeFont.className}>
-        <body>
-          <Component {...pageProps} />
-        </body>
-      </html>
-    </>
+    <html lang="en" className={font.className}>
+      <body>
+        {children}
+      </body>
+    </html>
   )
 }
