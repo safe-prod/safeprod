10be81104483745ca294998132ea7cb65c7ef4d9
SAFE <safeprod@protonmail.com>
2025-09-02 13:19:01 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -20,12 +20,32 @@ export default function Page() {
   }
   
   return (
-    <div className="m-8 flex flex-col">
+    <div className="m-4 flex flex-col">
       <div className="text-xl">
         <span className="font-bold">SAFE</span>
         <span className="pl-2 pr-2">/</span>
         <span>{ID}</span>
       </div>
+      
+      <div className="overflow-x-auto">
+			<table className="min-w-full border-collapse border border-gray-200">
+				<thead>
+					<tr className="bg-gray-100">
+						<th className="border border-gray-200 px-4 py-2 text-left">Transaction</th>
+						<th className="border border-gray-200 px-4 py-2 text-left">Debit</th>
+						<th className="border border-gray-200 px-4 py-2 text-left">Credit</th>
+					</tr>
+				</thead>
+				<tbody>
+					<tr>
+						<td className="border border-gray-200 px-4 py-2">prkfk34ss</td>
+						<td className="border border-gray-200 px-4 py-2">$100</td>
+						<td className="border border-gray-200 px-4 py-2"></td>
+					</tr>
+				</tbody>
+			</table>
+		</div>
+      
       <input
         type="text"
         value={inputText}
