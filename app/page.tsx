b3cca2e87970ea69c60490f41736abe21d97a534
SAFE <safeprod@protonmail.com>
2025-09-02 13:39:37 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,28 +1,13 @@
 "use client"
-import { Journal } from "./api/mcp/journal.ts"
 import { useState } from "react"
 
 export default function Page() {
   const ID = "ajde23dns"
-  
-  const [inputText, setInputText] = useState("")
-  const [responseText, setResponseText] = useState("")
-  const [tools, setTools] = useState("")
   const [journal, setJournal] = useState([
     { transaction: "Deposit", debit: 0, credit: 100 },
     { transaction: "Withdrawal", debit: 50, credit: 0 },
     { transaction: "Transfer", debit: 20, credit: 30 },
-  ]);
-
-  const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setInputText(event.target.value)
-  }
-
-  const handleButtonClick = async () => {
-    let { responseText, tools } = await Journal("openai/gpt-oss-20b", inputText)
-    setResponseText(responseText)
-    setTools(JSON.stringify(tools))
-  }
+  ])
   
   return (
     <div className="m-4 flex flex-col gap-y-4">
@@ -32,7 +17,7 @@ export default function Page() {
         <span>{ID}</span>
       </div>
       
-	  <table className="text-sm overflow-x-auto min-w-full">
+	  <table className="overflow-x-auto min-w-full">
 		<thead>
 		  <tr>
 		    <th className="px-4 py-2 text-left">Transaction</th>
@@ -50,17 +35,6 @@ export default function Page() {
           ))}
         </tbody>
 	  </table>
-		
-      <input
-        type="text"
-        value={inputText}
-        onChange={handleInputChange}
-        placeholder="Type your message here"
-      />
-      <button onClick={handleButtonClick}>Send</button>
-      <p>{responseText}</p>
-      <br/>
-      <p>{tools}</p>
-    </div>
+	</div>
   )
 }
