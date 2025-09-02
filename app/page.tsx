c9673f056e209ddb455bb87968eb49b5eee62380
SAFE <safeprod@protonmail.com>
2025-09-02 13:33:33 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -25,34 +25,32 @@ export default function Page() {
   }
   
   return (
-    <div className="m-4 flex flex-col">
+    <div className="m-4 flex flex-col gap-y-4">
       <div className="text-xl">
         <span className="font-bold">SAFE</span>
         <span className="pl-2 pr-2">/</span>
         <span>{ID}</span>
       </div>
       
-      <div className="text-sm overflow-x-auto">
-	    <table className="min-w-full">
-		  <thead>
-		    <tr>
-			  <th className="px-4 py-2 text-left">Transaction</th>
-			  <th className="px-4 py-2 text-left">Debit</th>
-			  <th className="px-4 py-2 text-left">Credit</th>
-			</tr>
-		  </thead>
-	      <tbody>
-            {journal.map((entry, index) => (
-              <tr key={index}>
-                <td className="px-4 py-2">{entry.transaction}</td>
-                <td className="px-4 py-2">{entry.debit}</td>
-                <td className="px-4 py-2">{entry.credit}</td>
-              </tr>
-            ))}
-          </tbody>
-		</table>
-	  </div>
-      
+	  <table className="text-sm overflow-x-auto min-w-full">
+		<thead>
+		  <tr>
+		    <th className="px-4 py-2 text-left">Transaction</th>
+			<th className="px-4 py-2 text-left">Debit</th>
+			<th className="px-4 py-2 text-left">Credit</th>
+		  </tr>
+		</thead>
+	    <tbody>
+          {journal.map((entry, index) => (
+            <tr key={index}>
+              <td className="px-4 py-2">{entry.transaction}</td>
+              <td className="px-4 py-2">{entry.debit}</td>
+              <td className="px-4 py-2">{entry.credit}</td>
+            </tr>
+          ))}
+        </tbody>
+	  </table>
+		
       <input
         type="text"
         value={inputText}
