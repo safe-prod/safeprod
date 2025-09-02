44ba6cf748529ac634b2acd818aa27681184c15f
SAFE <safeprod@protonmail.com>
2025-09-02 19:21:34 +0530

    Update journal.ts
    Testing

--- app/api/mcp/journal.ts
+++ app/api/mcp/journal.ts
@@ -3,6 +3,18 @@ import { createOpenRouter } from "@openrouter/ai-sdk-provider"
 import { StreamableHTTPClientTransport } from "@modelcontextprotocol/sdk/client/streamableHttp.js"
 import { z } from "zod"
 
+export const Coin = [
+  "toss_coin",
+  "Tosses a coin",
+  { sides: z.number().int().min(2) },
+  async ({ sides }) => {
+    const value = 1 + Math.floor(Math.random() * sides);
+      return {
+        content: [{ type: "text", text: `You tossed a ${value}!` }],
+      }
+  }
+]
+
 export const Journal = async (modelName: string, prompt: string) => {
   const openrouter = createOpenRouter({apiKey: "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"}) 
   
