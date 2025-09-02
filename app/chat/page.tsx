b5f770517952bad2213d24335742412dbf55a83b
SAFE <safeprod@protonmail.com>
2025-09-02 13:40:02 +0530

    Create chat/page.tsx

---
+++ app/chat/page.tsx
@@ -0,0 +1,34 @@
+"use client"
+import { Journal } from "../app/api/mcp/journal.ts"
+import { useState } from "react"
+
+export default function Page() {
+  const [inputText, setInputText] = useState("")
+  const [responseText, setResponseText] = useState("")
+  const [tools, setTools] = useState("")
+  
+  const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setInputText(event.target.value)
+  }
+
+  const handleButtonClick = async () => {
+    let {responseText, tools} = await Journal("openai/gpt-oss-20b", inputText)
+    setResponseText(responseText)
+    setTools(JSON.stringify(tools))
+  }
+  
+  return (
+    <div className="m-4 flex flex-col gap-y-4">
+      <input
+        type="text"
+        value={inputText}
+        onChange={handleInputChange}
+        placeholder="Type your message here"
+      />
+      <button onClick={handleButtonClick}>Send</button>
+      <p>{responseText}</p>
+      <br/>
+      <p>{tools}</p>
+    </div>
+  )
+}
