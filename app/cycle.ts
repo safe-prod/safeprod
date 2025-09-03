2f681ef1f01192207f71968ba63bf8b22ba5a817
SAFE <safeprod@protonmail.com>
2025-09-03 21:02:28 +0530

    Update cycle.ts

--- app/cycle.ts
+++ app/cycle.ts
@@ -2,7 +2,7 @@ import { streamText, MCPTransport, experimental_createMCPClient as createMCPClie
 import { createOpenRouter } from "@openrouter/ai-sdk-provider"
 import { StreamableHTTPClientTransport } from "@modelcontextprotocol/sdk/client/streamableHttp.js"
 
-export const Cycle = async (modelName: string, prompt: string) => {
+export const Cycle = async (prompt: string) => {
   const openrouter = createOpenRouter({apiKey: "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"}) 
   
   const url = new URL("https://safeprod.vercel.app/api/mcp")
@@ -12,7 +12,7 @@ export const Cycle = async (modelName: string, prompt: string) => {
   const tools = await mcpClient.tools()
 
   const response = streamText({
-    model: openrouter(modelName),
+    model: openrouter("deepseek/deepseek-chat-v3.1:free"),
     prompt: prompt,
     tools: tools
   })
