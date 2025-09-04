c7eadc1822c1c8f6c079965fa76bf70d43762fc2
SAFE <safeprod@protonmail.com>
2025-09-04 16:22:58 +0530

    Create database.ts

---
+++ app/api/mcp/database.ts
@@ -0,0 +1,26 @@
+import sqlite3 from "sqlite3"
+
+const db = new sqlite3.Database("database.db")
+
+db.exec(
+  `
+  CREATE TABLE IF NOT EXISTS journal (
+    id INTEGER PRIMARY KEY,
+    lineItem TEXT NOT NULL,
+    debit DECIMAL(10, 2) NOT NULL,
+    credit DECIMAL(10, 2) NOT NULL,
+  )
+  `,
+  (err) => {}
+)
+
+db.run(
+  `INSERT INTO
+   products(lineItem, debit, credit)
+   VALUES(?, ?, ?)
+  `,
+  ["Cash", 100, 0]
+  (err) => {}
+)
+
+db.close()
