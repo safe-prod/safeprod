8c37a233a4c2df6fbc3b4b083dd38914896e7942
SAFE <safeprod@protonmail.com>
2025-09-05 14:21:23 +0530

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,6 @@
-import "./gui/_style/globals.css"
+import "./_style/globals.css"
 import localFont from "next/font/local"
 import type { Metadata } from "next"
-import Link from "next/link"
 
 export const metadata: Metadata = {
   title: "SAFE",
@@ -10,8 +9,8 @@ export const metadata: Metadata = {
 
 const font = localFont({
   src: [
-    {path: "./gui/_style/regular.woff2", weight: "400"},
-    {path: "./gui/_style/bold.woff2", weight: "700"}
+    {path: "./_style/regular.woff2", weight: "400"},
+    {path: "./_style/bold.woff2", weight: "700"}
   ]
 })
 
@@ -21,21 +20,12 @@ export default function Layout({children}: {children: React.ReactNode}) {
   return (
     <html lang="en" className={font.className}>
       <head>
-        <link rel="icon" type="image/png" sizes="96x96" href="/gui/_style/icon.png" />
+        <link rel="icon" type="image/png" sizes="96x96" href="/_style/icon.png" />
       </head>
       <body>
         <div className="m-4 flex flex-col gap-y-4">
-         <div className="text-xl flex flex-row gap-x-4">
-            <div className="grow-8">
-              <span className="font-bold">SAFE</span>
-              <span className="pl-2 pr-2">/</span>
-              <span>{ID}</span>
-            </div>
-            <div className="grow-2 flex flex-row gap-x-2">
-              <Link href="/gui" className="grow">gui</Link>
-              <Link href="/api" className="grow">api</Link>
-            </div>
-          </div>
+          <div className="font-bold">SAFE</div>
+          <div>{ID}</div>
           {children}
         </div>
       </body>
