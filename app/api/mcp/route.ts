01af671e039d00b0ec42ac6f6cb97720a7bb6d0e
SAFE <safeprod@protonmail.com>
2025-09-05 14:29:22 +0530

    Update route.ts

--- app/api/mcp/route.ts
+++ app/api/mcp/route.ts
@@ -4,7 +4,7 @@ import fs from "fs"
 import path from "path"
 import { matrix } from "mathjs"
 
-export const Contract = () => {
+export const getContract = () => {
   let journal = JSON.parse(fs.readFileSync(path.join(process.cwd(), "app/api/mcp/_journal"), "utf8"))
   return {type: "SAFE", id: "asdf10", journal: journal}
 }
