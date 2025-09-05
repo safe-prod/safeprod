5d74a3762f5aa8ca372404024d0575b89007a100
SAFE <safeprod@protonmail.com>
2025-09-05 13:42:36 +0530

    Update route.ts

--- app/api/route.ts
+++ app/api/route.ts
@@ -1,10 +1,10 @@
 import fs from "fs"
 import path from "path"
 import { NextRequest } from "next/server"
-var nj = require("numjs")
+import { matrix } from "mathjs"
 
 export async function GET() {
-  let journal = nj.array(JSON.parse(fs.readFileSync(path.join(process.cwd(), "app/api/mcp/_journal"), "utf8")))
+  let journal = matrix(JSON.parse(fs.readFileSync(path.join(process.cwd(), "app/api/mcp/_journal"), "utf8")))
   const data = { message: journal.get(0) }
   return new Response(JSON.stringify(data), {status: 200, headers: {"Content-Type": "application/json"}})
 }
