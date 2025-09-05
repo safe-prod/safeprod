2d39c934551b87fd85ade8a725d9a5841cdab506
SAFE <safeprod@protonmail.com>
2025-09-05 14:40:51 +0530

    Update route.ts

--- app/api/mcp/route.ts
+++ app/api/mcp/route.ts
@@ -1,13 +1,5 @@
 import { createMcpHandler } from "mcp-handler"
 import { z } from "zod"
-import fs from "fs"
-import path from "path"
-import { matrix } from "mathjs"
-
-export const getContract = () => {
-  let journal = JSON.parse(fs.readFileSync(path.join(process.cwd(), "app/api/mcp/_journal"), "utf8"))
-  return {type: "SAFE", id: "asdf10", journal: journal}
-}
 
 const MCPServer = createMcpHandler(
   (server) => {
