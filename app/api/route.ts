722bc6e55a272d77cb9d6ae6e68acbfe3382b291
SAFE <safeprod@protonmail.com>
2025-09-05 14:23:36 +0530

    Update route.ts

--- app/api/route.ts
+++ app/api/route.ts
@@ -1,6 +1,8 @@
 import { NextRequest } from "next/server"
 
 export async function GET() {
-  const data = { message: "SAFE" }
-  return new Response(JSON.stringify(data), {status: 200, headers: {"Content-Type": "application/json"}})
+  return new Response(
+    JSON.stringify({"type": "SAFE", "id": "asdf12", "journal": [["Cash",100,0]]}),
+    {status: 200, headers: {"Content-Type": "application/json"}}
+  )
 }
