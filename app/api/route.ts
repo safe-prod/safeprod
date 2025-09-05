ac6f4ff7c0b66b647e96f3d3e7932af6649ee9dd
SAFE <safeprod@protonmail.com>
2025-09-05 13:27:56 +0530

    Update route.ts

--- app/api/route.ts
+++ app/api/route.ts
@@ -3,7 +3,7 @@ import path from "path"
 import { NextRequest } from "next/server"
 
 export async function GET() {
-  let journal = JSON.parse(fs.readFileSyncSync(path.join(process.cwd(), "app/api/mcp/_journal"), "utf8"))
+  let journal = JSON.parse(fs.readFileSync(path.join(process.cwd(), "app/api/mcp/_journal"), "utf8"))
   const data = { message: journal[0] }
   return new Response(JSON.stringify(data), {status: 200, headers: {"Content-Type": "application/json"}})
 }
