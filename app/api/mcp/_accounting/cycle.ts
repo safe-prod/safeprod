c171bdf3e29bf1513458a9f4a9a712c4f1200d7f
SAFE <safeprod@protonmail.com>
2025-09-05 14:41:57 +0530

    Update cycle.ts

--- app/api/mcp/_accounting/cycle.ts
+++ app/api/mcp/_accounting/cycle.ts
@@ -3,6 +3,6 @@ import path from "path"
 import { matrix } from "mathjs"
 
 export const getContract = () => {
-  let journal = JSON.parse(fs.readFileSync(path.join(process.cwd(), "app/api/mcp/_journal"), "utf8"))
+  let journal = JSON.parse(fs.readFileSync(path.join(process.cwd(), "app/api/mcp/_accounting/journal"), "utf8"))
   return {type: "SAFE", id: "asdf10", journal: journal}
 }
