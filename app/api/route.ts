e38cb29db54f427a528e8f307e8863d367652fa2
SAFE <safeprod@protonmail.com>
2025-09-05 13:34:37 +0530

    Update route.ts

--- app/api/route.ts
+++ app/api/route.ts
@@ -1,9 +1,10 @@
 import fs from "fs"
 import path from "path"
 import { NextRequest } from "next/server"
+let nj = require("numjs")
 
 export async function GET() {
-  let journal = JSON.parse(fs.readFileSync(path.join(process.cwd(), "app/api/mcp/_journal"), "utf8"))
-  const data = { message: journal[0] }
+  let journal = nj.array(JSON.parse(fs.readFileSync(path.join(process.cwd(), "app/api/mcp/_journal"), "utf8")))
+  const data = { message: journal.get(0) }
   return new Response(JSON.stringify(data), {status: 200, headers: {"Content-Type": "application/json"}})
 }
