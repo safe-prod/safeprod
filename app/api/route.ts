3963285ddc65695b6928d8d30ae24b7592a95db7
SAFE <safeprod@protonmail.com>
2025-09-06 14:33:49 +0530

    Update route.ts

--- app/api/route.ts
+++ app/api/route.ts
@@ -1,5 +1,9 @@
+import fs from "fs"
+import path from "path"
+import { streamText, MCPTransport, experimental_createMCPClient as createMCPClient } from "ai"
+import { createOpenRouter } from "@openrouter/ai-sdk-provider"
+import { StreamableHTTPClientTransport } from "@modelcontextprotocol/sdk/client/streamableHttp.js"
 import { NextRequest } from "next/server"
-import { getPDS } from "./lib/main.ts"
 
 export async function GET() {
   let PDS = getPDS()
@@ -7,4 +11,26 @@ export async function GET() {
     PDS,
     {status: 200, headers: {"Content-Type": "application/json"}}
   )
+}
+
+export const getPDS = () => {
+  let PDS = fs.readFileSync(path.join(process.cwd(), "app/api/PDS.md"), "utf8"))
+  return PDS
+}
+
+export const getResponse = async function* (prompt: string): AsyncGenerator<string> {
+  const url = new URL("https://safeprod.vercel.app/api/mcp")  
+  const client = await createMCPClient({transport: new StreamableHTTPClientTransport(url, { sessionId: "123" })})
+  const tools = await client.tools()
+  
+  const openrouter = createOpenRouter({apiKey: "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"}) 
+  const result = streamText({
+    model: openrouter("deepseek/deepseek-chat-v3.1:free"),
+    prompt: prompt,
+    tools: tools
+  })
+
+  for await (const textPart of result.textStream) {
+    yield textPart
+  }
 }
