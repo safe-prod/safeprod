e908e15e2450a48ac028f3e3d2be94ca04ffcc5f
SAFE <safeprod@protonmail.com>
2025-09-06 14:43:10 +0530

    Update route.ts

--- app/api/mcp/route.ts
+++ app/api/mcp/route.ts
@@ -3,16 +3,14 @@ import { z } from "zod"
 
 const handler = createMcpHandler(
   (server) => {
-    server.tool("getDocument", "Get document", {}, getDocument)
+    server.tool("getDocument", "Get document", {}, async () => {
+      return {
+        content: [{ type: "text", text: `Hello World`}],
+      }
+    })
   },
   {},
   { basePath: "/api" },
 )
-
-const getDocument = async () => {
-  return {
-    content: [{ type: "text", text: `Hello World`}],
-  }
-}
 
 export { handler as GET, handler as POST }
