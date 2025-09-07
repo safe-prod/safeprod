1c8fd1ca6f3a410a7d9221b71a03a9b0b24aa27e
SAFE <safeprod@protonmail.com>
2025-09-07 11:41:57 +0530

    Update route.ts

--- app/api/mcp/route.ts
+++ app/api/mcp/route.ts
@@ -4,7 +4,12 @@ import { getDocument } from "./fop/main.ts"
 
 const handler = createMcpHandler(
   (server) => {
-    server.tool("getDocument", "Get document", {}, getDocument)
+    // server.tool("getDocument", "Get document", {}, getDocument)
+    server.tool("getDocument", "Get document", {}, async () => {
+      return {
+        content: [{ type: "text", text: `This document describes the specifications of the new product.`}],
+      }
+    })
   },
   {},
   { basePath: "/api" },
