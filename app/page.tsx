9b1bea61e228027124b8a59e908142a83b48fcc8
SAFE <safeprod@protonmail.com>
2025-09-07 18:17:56 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 "use client"
 import { useState } from "react"
-import { Entrepreneur } from "./fop/entrepreneur"
+import Entrepreneur from "./fop/entrepreneur"
 
 export default function Page() {
   return (
