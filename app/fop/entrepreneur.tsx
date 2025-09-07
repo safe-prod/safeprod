2b45a3955a9f3493f67ca729ce8eb70dc4d7830b
SAFE <safeprod@protonmail.com>
2025-09-07 18:35:33 +0530

    Update entrepreneur.tsx

--- app/fop/entrepreneur.tsx
+++ app/fop/entrepreneur.tsx
@@ -9,7 +9,7 @@ They innovate, create new products or services, and drive economic growth by com
 
 "use client"
 import { useState } from "react"
-import { generateText } from "ai"
+import { streamText } from "ai"
 import { createOpenRouter } from "@openrouter/ai-sdk-provider"
 
 export default function Entrepreneur() {
@@ -17,13 +17,14 @@ export default function Entrepreneur() {
   const [output, setOutput] = useState("")
   const transferInput = async (e: any) => {
     setOutput("")
-    const currentOutput = await getOutput(input)
-    setOutput(currentOutput)
-    /*
-    for await (const newOutput of currentOutput) {
+    const openrouter = createOpenRouter({apiKey: process.env.NEXT_PUBLIC_OPENROUTER_API_KEY}) 
+    const currentOutput = streamText({
+      model: openrouter("openai/gpt-oss-120b:free"),
+      prompt: input
+    })
+    for await (const newOutput of currentOutput.textStream) {
       setOutput((output) => output + newOutput)
     }
-    */
   }
   return (
     <div className="flex flex-col">
@@ -32,20 +33,4 @@ export default function Entrepreneur() {
       <div>{output}</div>
     </div>
   )
-}
-
-// const getOutput = async function* (input: string): AsyncGenerator<string> {
-const getOutput = async function(input: string): Promise<string> {
-  const openrouter = createOpenRouter({apiKey: process.env.NEXT_PUBLIC_OPENROUTER_API_KEY}) 
-  const currentOutput = generateText({
-    model: openrouter("openai/gpt-oss-120b:free"),
-    prompt: input
-  })
-  await currentOutput.consumeStream()
-  return currentOutput
-  /*
-  for await (const newOutput of currentOutput.textStream) {
-    yield newOutput
-  }
-  */
 }
