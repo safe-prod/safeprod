af46e37a95e0d4904f0d5086d9351101d6e4ff27
SAFE <safeprod@protonmail.com>
2025-09-07 12:55:36 +0530

    Update main.ts

--- app/api/mcp/fop/main.ts
+++ app/api/mcp/fop/main.ts
@@ -1,6 +1,7 @@
 import { streamText, stepCountIs, MCPTransport, experimental_createMCPClient as createMCPClient } from "ai"
 import { createOpenRouter } from "@openrouter/ai-sdk-provider"
 import { StreamableHTTPClientTransport } from "@modelcontextprotocol/sdk/client/streamableHttp.js"
+import { z } from "zod"
 
 export const getResponse = async function* (prompt: string): AsyncGenerator<string> {
   const url = new URL("https://safeprod.vercel.app/api/mcp")  
@@ -10,8 +11,19 @@ export const getResponse = async function* (prompt: string): AsyncGenerator<stri
   const openrouter = createOpenRouter({apiKey: process.env.NEXT_PUBLIC_OPENROUTER_API_KEY}) 
   const { textStream, steps } = streamText({
     model: openrouter("google/gemini-2.0-flash-exp:free"),
-    tools: tools,
-    stopWhen: stepCountIs(2),
+    tools: {
+      weather: tool({
+        description: 'Get the weather in a location',
+        inputSchema: z.object({
+          location: z.string().describe('The location to get the weather for'),
+        }),
+        execute: async ({ location }) => ({
+          location,
+          temperature: 72 + Math.floor(Math.random() * 21) - 10,
+        })
+      })
+    },
+    stopWhen: stepCountIs(5),
     prompt: prompt
     /*
     messages: [
@@ -24,11 +36,5 @@ export const getResponse = async function* (prompt: string): AsyncGenerator<stri
 
   for await (const textPart of textStream) {
     yield textPart
-  }
-}
-
-export const getDocument = async () => {
-  return {
-    content: [{ type: "text", text: `This document describes the specifications of the new product.`}],
   }
 }
