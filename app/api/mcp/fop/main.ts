5eb5dc3738c8579e03776940f66da95f58d94f77
SAFE <safeprod@protonmail.com>
2025-09-07 13:32:43 +0530

    Update main.ts

--- app/api/mcp/fop/main.ts
+++ app/api/mcp/fop/main.ts
@@ -3,24 +3,22 @@ import { createOpenRouter } from "@openrouter/ai-sdk-provider"
 import { StreamableHTTPClientTransport } from "@modelcontextprotocol/sdk/client/streamableHttp.js"
 import { z } from "zod"
 
-const tools = {
-  weather: tool({
-    description: 'Get the weather in a location',
-    inputSchema: z.object({
-      location: z.string().describe('The location to get the weather for'),
-    }),
-    execute: async ({ location }) => ({
-      location,
-      temperature: 72000 + Math.floor(Math.random() * 21) - 10,
-    })
-  })
-}
-
 export const getResponse = async function* (prompt: string): AsyncGenerator<string> {
   const openrouter = createOpenRouter({apiKey: process.env.NEXT_PUBLIC_OPENROUTER_API_KEY}) 
   const { textStream, steps } = streamText({
     model: openrouter("google/gemini-2.0-flash-exp:free"),
-    tools: tools,
+    tools: {
+      weather: tool({
+        description: 'Get the weather in a location',
+        inputSchema: z.object({
+          location: z.string().describe('The location to get the weather for'),
+        }),
+        execute: async ({ location }) => ({
+          location,
+          temperature: 72000 + Math.floor(Math.random() * 21) - 10,
+        })
+      })
+    },
     stopWhen: stepCountIs(5),
     prompt: prompt
     /*
