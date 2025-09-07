abc49a22dc0e9f756bf3688a01c6a412271b0ae1
SAFE <safeprod@protonmail.com>
2025-09-07 13:09:25 +0530

    Update main.ts

--- app/api/mcp/fop/main.ts
+++ app/api/mcp/fop/main.ts
@@ -1,28 +1,12 @@
-import { streamText, tool, stepCountIs, MCPTransport, experimental_createMCPClient as createMCPClient } from "ai"
+import { streamText, stepCountIs, MCPTransport, experimental_createMCPClient as createMCPClient } from "ai"
 import { createOpenRouter } from "@openrouter/ai-sdk-provider"
 import { StreamableHTTPClientTransport } from "@modelcontextprotocol/sdk/client/streamableHttp.js"
-import { z } from "zod"
 
-export const getResponse = async function* (prompt: string): AsyncGenerator<string> {
-  const url = new URL("https://safeprod.vercel.app/api/mcp")  
-  const client = await createMCPClient({transport: new StreamableHTTPClientTransport(url, { sessionId: "123" })})
-  const tools = await client.tools()
-  
+export const getResponse = async function* (prompt: string, tools): AsyncGenerator<string> {
   const openrouter = createOpenRouter({apiKey: process.env.NEXT_PUBLIC_OPENROUTER_API_KEY}) 
   const { textStream, steps } = streamText({
     model: openrouter("google/gemini-2.0-flash-exp:free"),
-    tools: {
-      weather: tool({
-        description: 'Get the weather in a location',
-        inputSchema: z.object({
-          location: z.string().describe('The location to get the weather for'),
-        }),
-        execute: async ({ location }) => ({
-          location,
-          temperature: 72000 + Math.floor(Math.random() * 21) - 10,
-        })
-      })
-    },
+    tools: tools,
     stopWhen: stepCountIs(5),
     prompt: prompt
     /*
@@ -38,3 +22,9 @@ export const getResponse = async function* (prompt: string): AsyncGenerator<stri
     yield textPart
   }
 }
+
+/*
+const url = new URL("https://safeprod.vercel.app/api/mcp")  
+  const client = await createMCPClient({transport: new StreamableHTTPClientTransport(url, { sessionId: "123" })})
+  const tools = await client.tools()
+*/
