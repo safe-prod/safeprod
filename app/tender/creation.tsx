354eec7179c2457269357a4d9920fb375502f9df
SAFE <safeprod@protonmail.com>
2025-09-08 12:22:52 +0530

    Update and rename test.tsx to creation.tsx

---
+++ app/tender/creation.tsx
@@ -0,0 +1,27 @@
+"use client"
+import "dotenv/config"
+import { streamText } from "ai"
+import { useState } from "react"
+
+export default async function TenderCreation() {
+  const [input, setInput] = useState("")
+  const [output, setOutput] = useState("")
+  return (
+    <div className="flex flex-col">
+      <textarea value={input} onChange={(e) => {setInput(e.target.value)}} />
+      <div
+        onClick={() => {
+          const result = streamText({
+            model: "meta/llama-3.1-8b",
+            prompt: input
+          })
+          await result.consumeStream()
+          setOutput(await result.text)
+        }}
+      >
+        Create
+      </div>
+      {text}
+    </div>
+  )
+}
