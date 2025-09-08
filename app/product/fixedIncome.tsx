c8885ca8ee2c65905e6d02ff9e51d70c1769250a
SAFE <safeprod@protonmail.com>
2025-09-08 12:11:59 +0530

    Create fixedIncome.tsx

---
+++ app/product/fixedIncome.tsx
@@ -0,0 +1,12 @@
+/***
+
+# Fixed Income
+
+Fixed-income instruments are debt instruments that can be settled in cash and for which the counterparty is an investor or a bank.
+
+Loans are debt instruments formed and governed by a private agreement usually between an individual or company and a financial intermediary, such as a bank.
+
+Bonds or fixed-income securities are more standardized contractual agreements between larger issuers and investors.
+
+***/
+
