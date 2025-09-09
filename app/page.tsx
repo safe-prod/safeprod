de8393aa7222a153316e8b78cb4f0f981887235f
SAFE <safeprod@protonmail.com>
2025-09-09 14:38:48 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -5,9 +5,10 @@ export default function Page() {
   return (
     <div className="flex flex-col">
       <div className="mt-4 mb-8 text-red-900 text-5xl font-bold">Welcome</div>
-      <input className="sticky top-2 backdrop-blur-sm mb-4 border-none rounded-lg p-2 bg-gray-50/50 text-red-900 text-3xl font-bold" placeholder="Search..." />
+      <input className="sticky top-2 backdrop-blur-sm mb-4 border-none rounded-lg p-2 bg-gray-50/50 text-red-900 caret-red-900 text-3xl font-bold" placeholder="Search..." />
+      <div className="scroll-smooth scroll-y">
       {items.map((_, index) => (
-        <div key={index} className="p-2 mb-2 rounded-lg flex flex-col bg-gray-200">
+        <div key={index} className="snap-start p-2 mb-2 rounded-lg flex flex-col bg-gray-200">
           <div className="mb-2 flex flex-row grow font-bold text-xl">
             <div className="text-red-950 grow">Investor</div>
             <div className="text-red-800 grow text-right">$1,500</div>
@@ -18,7 +19,8 @@ export default function Page() {
           </div>
         </div>
       ))}
-      <div className="fixed bottom-4 right-4 bg-red-900/70 backdrop-blur-sm text-white font-bold text-xl rounded-xl p-4 shadow-sm shadow-red-500/50">
+      </div>
+      <div className="fixed bottom-4 right-4 bg-red-900/90 backdrop-blur-sm text-white font-bold text-xl rounded-xl p-4">
         &nbsp;+&nbsp;
       </div>
     </div>
