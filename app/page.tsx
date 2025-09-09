1c0557390ca4e08c27c29f8ae548678c64155a5a
SAFE <safeprod@protonmail.com>
2025-09-09 18:07:31 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -6,12 +6,12 @@ export default function Page() {
     <div className="flex flex-col">
       <div className="scroll-smooth">
       {items.map((_, index) => (
-        <div key={index} className="p-2 mb-2 rounded-lg flex flex-col border border-gray-100">
+        <div key={index} className="p-2 mb-2 rounded-sm flex flex-col border border-gray-100">
           <div className="mb-2 flex flex-row grow font-bold text-xl">
             <div className="text-black grow">Investor</div>
             <div className="text-red-900 grow text-right">$1,500</div>
           </div>
-          <div className="flex flex-row grow font-bold text-sm text-gray-600">
+          <div className="flex flex-row grow font-bold text-sm text-gray-900">
             <div className="grow-8">Sep 27, 5:03PM</div>
             <div className="grow-2 max-w-24 text-right">Equity</div>
           </div>
@@ -19,12 +19,12 @@ export default function Page() {
       ))}
       </div>
       <div className="fixed bottom-0 flex flex-col left-0 right-0 ml-2 mr-2">
-        <div className="backdrop-blur-md bg-white/30 p-2 rounded-lg flex flex-col border border-gray-100">
+        <div className="backdrop-blur-md bg-white/30 p-2 rounded-sm flex flex-col border border-gray-100">
           <div className="mb-2 flex flex-row grow font-bold text-xl">
-            <input type="text" className="w-1/2 text-black border-b ml-2 border-red-900" placeholder="Investor" />
+            <input type="text" className="w-1/2 text-black border-b mr-2 border-red-900" placeholder="Investor" />
             <input type="text" className="w-1/2 text-red-900 border-b border-red-900 text-right" placeholder="$" />
           </div>
-          <div className="flex flex-row grow font-bold text-sm text-gray-600">
+          <div className="flex flex-row grow font-bold text-sm text-gray-900">
             <div className="grow-8">Sep 27, 5:03PM</div>
             <div className="grow-2 max-w-24 text-right">Equity</div>
           </div>
