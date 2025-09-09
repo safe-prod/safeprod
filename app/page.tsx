fa06b3e69626b4848c1da7d75dee763e095801a4
SAFE <safeprod@protonmail.com>
2025-09-09 18:19:09 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -4,9 +4,21 @@ export default function Page() {
   const items = Array.from({ length: 20 })
   return (
     <div className="flex flex-col">
-      <div className="scroll-smooth">
+      <div className="sticky top-0 flex flex-col left-0 right-0 ml-2 mr-2">
+        <div className="backdrop-blur-md bg-white/50 p-2 flex flex-col border border-gray-100">
+          <div className="mb-2 flex flex-row grow font-bold text-xl">
+            <div className="text-black grow">Initial</div>
+            <div className="text-red-900 grow text-right">$500</div>
+          </div>
+          <div className="flex flex-row grow font-bold text-sm text-gray-900">
+            <div className="grow-8">Sep 10, 8:34PM</div>
+            <div className="grow-2 max-w-24 text-right">SAFE</div>
+          </div>
+        </div>
+      </div>
+      <div className="scroll-smooth snap-y">
       {items.map((_, index) => (
-        <div key={index} className="p-2 flex flex-col border-b border-gray-200">
+        <div key={index} className="snap-end p-2 flex flex-col border-b border-gray-200">
           <div className="mb-2 flex flex-row grow font-bold text-xl">
             <div className="text-black grow">Investor</div>
             <div className="text-red-900 grow text-right">$1,500</div>
@@ -19,7 +31,7 @@ export default function Page() {
       ))}
       </div>
       <div className="fixed bottom-0 flex flex-col left-0 right-0 ml-2 mr-2">
-        <div className="backdrop-blur-md bg-white/60 p-2 rounded-sm flex flex-col border border-gray-100">
+        <div className="backdrop-blur-md bg-white/50 p-2 flex flex-col border border-gray-100">
           <div className="mb-2 flex flex-row grow font-bold text-xl">
             <input type="text" className="w-1/2 text-black border-b mr-2 border-red-900" placeholder="Investor" />
             <input type="text" className="w-1/2 text-red-900 border-b border-red-900 text-right" placeholder="$" />
