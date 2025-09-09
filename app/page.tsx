82e655fae30635a1a0401a50262611d26024150f
SAFE <safeprod@protonmail.com>
2025-09-09 13:05:12 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -2,8 +2,8 @@
 
 export default function Page() {
   return (
-    <div>
-      
+    <div className="m-4 flex flex-col bg-gray-500">
+      <div className="text-red-200 text-3xl font-bold">Fixed Income</div>
     </div>
   )
 }
