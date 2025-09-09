c4b12e1480dceaf41416cb8a166964466db66fa0
SAFE <safeprod@protonmail.com>
2025-09-09 18:24:39 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -4,8 +4,8 @@ export default function Page() {
   const items = Array.from({ length: 20 })
   return (
     <div className="flex flex-col">
-      <div className="sticky top-0 flex flex-col left-0 right-0 ml-2 mr-2">
-        <div className="backdrop-blur-md bg-white/50 p-2 flex flex-col border border-gray-100">
+      <div className="sticky top-0 flex flex-col left-0 right-0">
+        <div className="backdrop-blur-md bg-white/50 p-2 flex flex-col border-b border-gray-200">
           <div className="mb-2 flex flex-row grow font-bold text-xl">
             <div className="text-black grow">Initial</div>
             <div className="text-red-900 grow text-right">$500</div>
@@ -16,7 +16,7 @@ export default function Page() {
           </div>
         </div>
       </div>
-      <div className="scroll-smooth snap-y">
+      <div className="scroll-smooth snap-ysnap-mandatory">
       {items.map((_, index) => (
         <div key={index} className="snap-end p-2 flex flex-col border-b border-gray-200">
           <div className="mb-2 flex flex-row grow font-bold text-xl">
@@ -31,7 +31,7 @@ export default function Page() {
       ))}
       </div>
       <div className="fixed bottom-0 flex flex-col left-0 right-0 ml-2 mr-2">
-        <div className="backdrop-blur-md bg-white/50 p-2 flex flex-col border border-gray-100">
+        <div className="backdrop-blur-md bg-white/50 p-2 flex flex-col border-t border-gray-200">
           <div className="mb-2 flex flex-row grow font-bold text-xl">
             <input type="text" className="w-1/2 text-black border-b mr-2 border-red-900" placeholder="Investor" />
             <input type="text" className="w-1/2 text-red-900 border-b border-red-900 text-right" placeholder="$" />
