6263d4e70e615f57cf975bb2ec9aa8c60210a5b0
SAFE <safeprod@protonmail.com>
2025-09-09 14:08:09 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -8,14 +8,17 @@ export default function Page() {
       <input className="mb-4 border-none rounded-lg p-2 bg-gray-50 text-red-800 text-xl font-bold" placeholder="Search..." />
       {items.map((_, index) => (
         <div key={index} className="p-2 mb-2 rounded-lg flex flex-col bg-gray-200">
-          <div className="flex flex-row grow font-bold text-xl">
+          <div className="m-2 flex flex-row grow font-bold text-xl">
             <div className="text-red-900 grow">Grocery Store</div>
             <div className="text-red-800 grow text-right">($100)</div>
           </div>
-          <div className="rounded-2xl max-w-24 text-center font-bold text-sm bg-gray-300 text-red-900">Money</div>
+          <div className="flex flex-row grow font-bold text-sm">
+            <div className="text-red-900 grow">Sep 27, 5:03PM</div>
+            <div className="rounded-2xl max-w-24 text-center bg-gray-300 text-red-900">Money</div>
+          </div>
         </div>
       ))}
-      <div className="fixed bottom-2 right-2 bg-red-800 text-white font-bold text-xl rounded-xl p-8 shadow-lg">
+      <div className="fixed bottom-2 right-2 bg-red-700 text-white font-bold text-xl rounded-xl p-4 shadow-lg shadow-red-500">
         +
       </div>
     </div>
