9ff0e54fb967ea0e2088875f47f169ede0a19ec4
SAFE <safeprod@protonmail.com>
2025-09-09 14:24:59 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,16 @@
 "use client"
 
 export default function Page() {
-  const items = Array.from({ length: 10 })
+  const items = Array.from({ length: 20 })
   return (
     <div className="flex flex-col">
       <div className="mt-4 mb-8 text-red-900 text-5xl font-bold">Welcome</div>
-      <input className="sticky top-0 backdrop-blur-sm mb-4 border-none rounded-lg p-2 bg-gray-50/50 text-red-800 text-xl font-bold" placeholder="Search..." />
+      <input className="sticky top-2 backdrop-blur-sm mb-4 border-none rounded-lg p-2 bg-gray-50/50 text-red-800 text-xl font-bold" placeholder="Search..." />
       {items.map((_, index) => (
         <div key={index} className="p-2 mb-2 rounded-lg flex flex-col bg-gray-200">
           <div className="mb-2 flex flex-row grow font-bold text-xl">
             <div className="text-red-900 grow">Grocery Store</div>
-            <div className="text-red-800 grow text-right">($100)</div>
+            <div className="text-red-700 grow text-right">($100)</div>
           </div>
           <div className="flex flex-row grow font-bold text-sm text-red-950">
             <div className="grow-8">Sep 27, 5:03PM</div>
@@ -18,7 +18,7 @@ export default function Page() {
           </div>
         </div>
       ))}
-      <div className="fixed bottom-4 right-4 bg-red-700 text-white font-bold text-xl rounded-xl p-4 shadow-lg shadow-red-500/50">
+      <div className="fixed bottom-4 right-4 bg-red-700/50 backdrop-blur-sm text-white font-bold text-xl rounded-xl p-4 shadow-sm shadow-red-500/50">
         &nbsp;+&nbsp;
       </div>
     </div>
