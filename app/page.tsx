47ebe87afffa8c5a1d9422b5aac8cc463ad6f24c
SAFE <safeprod@protonmail.com>
2025-09-09 18:04:49 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,7 @@ export default function Page() {
     <div className="flex flex-col">
       <div className="scroll-smooth">
       {items.map((_, index) => (
-        <div key={index} className="p-2 mb-2 rounded-lg shadow-sm flex flex-col border border-gray-50">
+        <div key={index} className="p-2 mb-2 rounded-lg flex flex-col border border-gray-100">
           <div className="mb-2 flex flex-row grow font-bold text-xl">
             <div className="text-black grow">Investor</div>
             <div className="text-red-900 grow text-right">$1,500</div>
@@ -19,10 +19,10 @@ export default function Page() {
       ))}
       </div>
       <div className="fixed bottom-0 flex flex-col left-0 right-0 ml-2 mr-2">
-        <div className="backdrop-blur-md bg-white/30 p-2 rounded-lg shadow-sm flex flex-col border border-gray-50">
+        <div className="backdrop-blur-md bg-white/30 p-2 rounded-lg flex flex-col border border-gray-100">
           <div className="mb-2 flex flex-row grow font-bold text-xl">
-            <div><input type="text" className="text-black border-b ml-2 border-red-900" placeholder="Investor" /></div>
-            <div><input type="text" className="text-red-900 border-b border-red-900 text-right" placeholder="$" /></div>
+            <input type="text" className="w-1/2 text-black border-b ml-2 border-red-900" placeholder="Investor" />
+            <input type="text" className="w-1/2 text-red-900 border-b border-red-900 text-right" placeholder="$" />
           </div>
           <div className="flex flex-row grow font-bold text-sm text-gray-600">
             <div className="grow-8">Sep 27, 5:03PM</div>
