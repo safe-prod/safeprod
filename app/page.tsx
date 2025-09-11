6b4b93adcc32f6f9bbc9fae9a7beb1fd73f26789
SAFE <safeprod@protonmail.com>
2025-09-11 13:51:43 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -20,13 +20,13 @@ Tender meaning:
 import { useState } from "react"
 
 const itemsData = [
-  { counterparty: "Investor 1", amount: "$1,500", date: "Sep 27, 5:03PM", product: "Equity" },
-  { counterparty: "Grocery Store", amount: "$2,500", date: "Sep 27, 5:03PM", product: "Equity" },
-  { counterparty: "Investor 1", amount: "$1,500", date: "Sep 27, 5:03PM", product: "Equity" },
-  { counterparty: "Investor 1", amount: "$1,500", date: "Sep 27, 5:03PM", product: "Equity" },
-  { counterparty: "Investor 1", amount: "$1,500", date: "Sep 27, 5:03PM", product: "Equity" },
-  { counterparty: "Investor 1", amount: "$1,500", date: "Sep 27, 5:03PM", product: "Equity" },
-  { counterparty: "Investor 1", amount: "$1,500", date: "Sep 27, 5:03PM", product: "Equity" },
+  { counterparty: "Investor", amount: "$1,500", date: "Sep 27, 5:03PM", product: "Equity" },
+  { counterparty: "Grocery Store", amount: "$98", date: "Sep 28, 5:03PM", product: "Money" },
+  { counterparty: "Restaurant", amount: "$100", date: "Sep 27, 12:33PM", product: "Money" },
+  { counterparty: "Creditor", amount: "$520", date: "Sep 27, 5:03PM", product: "Debt" },
+  { counterparty: "Investor", amount: "$1,500", date: "Sep 29, 5:03PM", product: "Equity" },
+  { counterparty: "Investor", amount: "$1,500", date: "Sep 27, 5:03PM", product: "Equity" },
+  { counterparty: "Grocery Store", amount: "$120", date: "Sep 27, 5:03PM", product: "Money" },
   { counterparty: "Investor 1", amount: "$1,500", date: "Sep 27, 5:03PM", product: "Equity" },
   { counterparty: "Investor 1", amount: "$1,500", date: "Sep 27, 5:03PM", product: "Equity" },
   { counterparty: "Investor 1", amount: "$1,500", date: "Sep 27, 5:03PM", product: "Equity" },
@@ -43,14 +43,14 @@ export default function page() {
   return (
     <div className="flex flex-col bg-white">
       <div className="sticky top-0 flex flex-col left-0 right-0">
-        <div className="backdrop-blur-md bg-white/50 p-2 flex flex-col border-b border-gray-100">
-          <div className="mb-2 flex flex-row grow font-bold text-lg">
-            <div className="text-black grow">Initial</div>
-            <div className="text-red-900 grow text-right">$250,000</div>
+        <div className="backdrop-blur-md bg-white/50 p-2 flex flex-col border-b border-gray-100 font-bold">
+          <div className="mb-2 flex flex-row">
+            <div className="w-1/2 text-black text-lg">Initial</div>
+            <div className="w-1/2 text-sm text-gray-600 text-right">SAFE</div>
           </div>
-          <div className="flex flex-row grow font-bold text-sm text-gray-600">
-            <div className="w-1/2">Sep 12, 8:57PM</div>
-            <div className="w-1/2 text-right">SAFE</div>
+          <div className="flex flex-row">
+            <div className="w-1/2 text-red-900 grow text-lg text-right">$250,000</div>
+            <div className="w-1/2 text-sm text-gray-600">Sep 12, 8:57PM</div>
           </div>
         </div>
       </div>
@@ -72,11 +72,11 @@ export default function page() {
         <div className="backdrop-blur-md bg-white/50 p-2 flex flex-col border-t border-gray-100">
           <div className="mb-2 flex flex-row grow font-bold text-lg">
             <input type="text" onChange={e => setItems(searchCounterparty(e.target.value))} className="w-1/2 text-black mr-2 focus:outline-none" placeholder="Investor" />
-            <input type="text" className="w-1/2 text-red-900 focus:outline-none text-right" placeholder="$" />
+            <input type="text" onChange={e => setItems(searchAmount(e.target.value))} className="w-1/2 text-red-900 focus:outline-none text-right" placeholder="$" />
           </div>
           <div className="flex flex-row grow font-bold text-sm text-gray-600">
-            <input type="text" className="w-1/2 mr-2 focus:outline-none" placeholder="Sep 27, 5:03PM" />
-            <input type="text" className="w-1/2 focus:outline-none text-right" placeholder="Equity" />
+            <input type="text" onChange={e => setItems(searchDate(e.target.value))} className="w-1/2 mr-2 focus:outline-none" placeholder="Sep 27, 5:03PM" />
+            <input type="text" onChange={e => setItems(searchProduct(e.target.value))} className="w-1/2 focus:outline-none text-right" placeholder="Equity" />
           </div>
         </div>
       </div>
@@ -85,7 +85,19 @@ export default function page() {
 }
 
 function searchCounterparty(search: any): any {
-  return itemsData.filter(item => item.counterparty.startsWith(search))
+  return itemsData.filter(item => item.counterparty.includes(search))
+}
+
+function searchAmount(search: any): any {
+  return itemsData.filter(item => item.amount.includes(search))
+}
+
+function searchProduct(search: any): any {
+  return itemsData.filter(item => item.product.includes(search))
+}
+
+function searchDate(search: any): any {
+  return itemsData.filter(item => item.date.includes(search))
 }
 
 /**
