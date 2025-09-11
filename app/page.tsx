9ac7a91d02bb586127e0d503abc9ebd2c757b059
SAFE <safeprod@protonmail.com>
2025-09-11 14:43:29 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -40,15 +40,15 @@ let paymentsData = [
 
 let style = {
   payments: "flex flex-col scroll-smooth snap-y snap-mandatory bg-white",
-  payment: "p-2 flex flex-col border-b border-gray-100 snap-end font-bold align-middle",  
+  payment: "p-2 flex flex-col border-b border-gray-100 snap-end font-bold",  
   initial: "sticky top-0 left-0 right-0 backdrop-blur-md bg-white/50",
-  new: "fixed bottom-0 left-0 right-0 backdrop-blur-md bg-white/50",
+  new: "fixed bottom-0 left-0 right-0 border-t backdrop-blur-md bg-white/50",
   firstRow: "mb-2 flex flex-row",
   secondRow: "flex flex-row",
-  counterparty: "w-1/2 mr-2 text-lg text-black",
-  product: "w-1/2 text-right text-sm text-gray-600",
-  amount: "w-1/2 mr-2 text-lg text-red-900",
-  date: "w-1/2 text-right text-sm text-gray-600",
+  counterparty: "w-1/2 h-full align-middle mr-2 text-lg text-black",
+  product: "w-1/2 h-full align-middle text-right text-sm text-gray-600",
+  amount: "w-1/2 h-full align-middle mr-2 text-lg text-red-900",
+  date: "w-1/2 h-full align-middle text-right text-sm text-gray-600",
   input: "focus:outline-none"
 }
 
