9142a15b6f320dc39a76ba1513ed1cd554ba086e
SAFE <safeprod@protonmail.com>
2025-09-13 13:37:47 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,21 +1,3 @@
-/***
-
-# Payment
-
-Pay meaning:
-- From Middle English payen, from Old French paiier (“pay”), from Medieval Latin pācāre (“to *settle*, satisfy”) from Latin pācāre (“to pacify”).
-- To discharge a debt or other obligation, by giving or doing what is due or required.
-- To give money or other compensation to in exchange for goods or services.
-- To give something else than money.
-- To suffer consequences. 
-- Note: The two concepts of buy and sell can be replaced by the single concept of pay. It has more weight when said that a person pays another person with a stock, rather than saying a person buys a stock from another person. The notions of buyer and seller can be replaced with the morphologically simpler payer and payee. What is being paid is called a financial product e.g. money, bond, stock etc.
-
-Tender meaning:
-- Any offer made to *settle a debt*, which may include cash, securities, or other forms of payment. This usage emphasizes the act of offering something to fulfill a financial obligation. Comes from legal tender.
-- A formal offer to buy or sell something.
-
-***/
-
 "use client"
 import { useState } from "react"
 
