19d636f71d229816fed57e638e1f86d2b50e8126
SAFE <safeprod@protonmail.com>
2025-09-13 13:38:34 +0530

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,15 +1,3 @@
-/***
-
-# Finance
-
-Meaning:
-
-- Derived from Middle English finaunce, from finer (“to pay ransom”), from English fine (“to pay a penalty”)), from *fin (“end”)*, from Latin fīnis.
-- Original English sense that appeared c. 1400 was “ending”. The sense of *ending or satisfying a debt* originated from French influence.
-- The management of money and other assets.
-
-***/
-
 import "./style/globals.css"
 import localFont from "next/font/local"
 import type { Metadata } from "next"
