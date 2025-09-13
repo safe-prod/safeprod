7ef4270e8143656a7257d3c5dbf5ebf92cc6ac49
SAFE <safeprod@protonmail.com>
2025-09-14 00:00:35 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,22 +1,22 @@
 "use client"
 import { useState } from "react"
 import { style } from "./style/style.ts"
-import { getPayments, searchCounterparty, searchAmount, searchDate, searchProduct } from "./script/database.ts" 
+import { getPayments, searchCounterparty, searchFop, searchDate, searchProduct } from "./script/database.ts" 
 
 let payments = await getPayments()
 
 export default async function page() {
-  // const [payments, setPayments] = useState(paymentsData)
+  const [payments, setPayments] = useState(payments)
   
   return (
     <div className={`${style.payments}`}>
       <div className={`${style.initial} ${style.payment}`}>
         <div className={`${style.firstRow}`}>
-          <div className={`${style.fop}`}>Initial</div>
+          <div className={`${style.counterparty}`}>SAFE</div>
           <div className={`${style.product}`}>SAFE</div>
         </div>
         <div className={`${style.secondRow}`}>
-          <div className={`${style.amount}`}>250,000</div>
+          <div className={`${style.fop}`}>250,000</div>
           <div className={`${style.date}`}>Sep 12, 8:57PM</div>
         </div>
       </div>
@@ -26,11 +26,11 @@ export default async function page() {
         {payments.map((payment: any) => (
           <div key={payment.id} className={`${style.payment}`}>
             <div className={`${style.firstRow}`}>
-              <div className={`${style.fop}`}>{payment.fop}</div>
+              <div className={`${style.counterparty}`}>{payment.counterparty}</div>
               <div className={`${style.product}`}>{payment.product}</div>
             </div>
             <div className={`${style.secondRow}`}>
-              <div className={`${style.amount}`}>{payment.amount}</div>
+              <div className={`${style.fop}`}>{payment.fop}</div>
               <div className={`${style.date}`}>{payment.date}</div>
             </div>
           </div>
@@ -39,11 +39,11 @@ export default async function page() {
       {/*
       <div className={`${style.new} ${style.payment}`}>
         <div className={`${style.firstRow}`}>
-          <input type="text" onChange={e => setPayments(searchCounterparty(e.target.value))} className={`${style.fop} ${style.input}`} placeholder="" />
+          <input type="text" onChange={e => setPayments(searchCounterparty(e.target.value))} className={`${style.counterparty} ${style.input}`} placeholder="" />
           <input type="text" onChange={e => setPayments(searchProduct(e.target.value))} className={`${style.product} ${style.input}`} placeholder="" />
         </div>
         <div className={`${style.secondRow}`}>
-          <input type="text" onChange={e => setPayments(searchAmount(e.target.value))} className={`${style.amount} ${style.input}`} placeholder="" />
+          <input type="text" onChange={e => setPayments(searchFop(e.target.value))} className={`${style.fop} ${style.input}`} placeholder="" />
           <input type="text" onChange={e => setPayments(searchDate(e.target.value))} className={`${style.date} ${style.input}`} placeholder="" />
         </div>
       </div>
