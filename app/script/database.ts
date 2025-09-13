90602b3c2423455ea870129ae742bf27b6641a6a
SAFE <safeprod@protonmail.com>
2025-09-13 23:22:38 +0530

    Update database.ts

--- app/script/database.ts
+++ app/script/database.ts
@@ -1,6 +1,6 @@
 import { createClient } from "@supabase/supabase-js"
 
-let payments = []
+let payments: any = []
 const supabase = createClient(`${process.env.NEXT_PUBLIC_SUPABASE_URL}`, `${process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY}`);
 
 export async function getPayments() {
