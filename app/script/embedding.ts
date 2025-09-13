05de1916ac225eb5f912ded35bfde1440a26f0bd
SAFE <safeprod@protonmail.com>
2025-09-13 23:01:51 +0530

    Update embedding.ts

--- app/script/embedding.ts
+++ app/script/embedding.ts
@@ -1,6 +1,6 @@
 import { embed } from "ai"
 
-export async function getEmbedding(value) {
+export async function getEmbedding(value: any): any {
   const result = await embed({
     model: `${process.env.NEXT_PUBLIC_EMBEDDING_MODEL}`,
     value: value
