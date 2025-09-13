2b7baa6a31fbf6438df8092292398b1b18a1d005
SAFE <safeprod@protonmail.com>
2025-09-13 23:15:54 +0530

    Update database.ts

--- app/script/database.ts
+++ app/script/database.ts
@@ -1,8 +1,26 @@
 import { createClient } from "@supabase/supabase-js"
 
+let payments = []
 const supabase = createClient(`${process.env.NEXT_PUBLIC_SUPABASE_URL}`, `${process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY}`);
 
 export async function getPayments() {
   const { data } = await supabase.from("safeprod").select()
-  return Array(data)
+  payments = Array(data)
+  return payments
+}
+
+export function searchCounterparty(search: any): any {
+  return payments.filter((payment: any) => payment.counterparty.includes(search))
+}
+
+export function searchProduct(search: any): any {
+  return payments.filter((payment: any) => payment.product.includes(search))
+}
+
+export function searchAmount(search: any): any {
+  return payments.filter((payment: any) => payment.amount.includes(search))
+}
+
+export function searchDate(search: any): any {
+  return payments.filter((payment: any) => payment.date.includes(search))
 }
