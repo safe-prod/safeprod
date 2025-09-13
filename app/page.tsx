616066c23e173d8e1e24b041197eb1593cb011e9
SAFE <safeprod@protonmail.com>
2025-09-13 13:57:30 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,29 +1,16 @@
 "use client"
 import { useState } from "react"
-import { embed } from "ai"
-
+import { style } from "./style/style.ts"
+// import { embed } from "ai"
+/*
 const result = await embed({
   model: "google/text-embedding-005",
   value: "Grocery Store"
 })
-
+*/
 let paymentsData = [
-  { counterparty: "Grocery Store", amount: JSON.stringify(result.embedding), date: "Sep 28, 5:03PM", product: "Money" }
+  { counterparty: "Grocery Store", amount: "JSON.stringify(result.embedding)", date: "Sep 28, 5:03PM", product: "Money" }
 ]
-
-let style = {
-  payments: "flex flex-col scroll-smooth snap-y snap-mandatory bg-white",
-  payment: "p-2 flex flex-col border-b border-gray-100 snap-end font-bold",  
-  initial: "sticky top-0 left-0 right-0 backdrop-blur-md bg-white/50",
-  new: "fixed bottom-0 left-0 right-0 border-t backdrop-blur-md bg-white/50",
-  firstRow: "mb-2 flex flex-row",
-  secondRow: "flex flex-row",
-  counterparty: "w-1/2 mr-2 text-lg text-black",
-  product: "w-1/2 text-right text-sm text-gray-600",
-  amount: "w-1/2 mr-2 text-lg text-red-900",
-  date: "w-1/2 text-right text-sm text-gray-600",
-  input: "focus:outline-none"
-}
 
 export default function page() {
   const [payments, setPayments] = useState(paymentsData)
