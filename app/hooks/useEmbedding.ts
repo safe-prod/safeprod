ed4a3b96d78df2c2dab92b511a3260c1192bd00d
SAFE <safeprod@protonmail.com>
2025-09-16 13:14:18 +0530

    Update and rename embedding.ts to useEmbedding.ts

--- app/hooks/useEmbedding.ts
+++ app/hooks/useEmbedding.ts
@@ -1,6 +1,6 @@
 import { embed } from "ai"
 
-export async function getEmbedding(value: any) {
+export default async function getEmbedding(value: any) {
   const { embedding } = await embed({
     model: `${process.env.NEXT_PUBLIC_EMBEDDING_MODEL}`,
     value: value
