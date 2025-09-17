5e1580e9e9e840a2ec006bfd6d67f8721c75a26b
SAFE <safeprod@protonmail.com>
2025-09-17 12:29:02 +0530

    Delete app/hooks/useEmbedding.ts

--- app/hooks/useEmbedding.ts
+++
@@ -1,9 +0,0 @@
-import { embed } from "ai"
-
-export default async function useEmbedding(value: any) {
-  const { embedding } = await embed({
-    model: `${process.env.NEXT_PUBLIC_EMBEDDING_MODEL}`,
-    value: value
-  })
-  return embedding
-}
