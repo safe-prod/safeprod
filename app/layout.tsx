2c7e5f9918f13738465423dff048040bd5b71d62
SAFE <safeprod@protonmail.com>
2025-09-17 15:37:09 +0530

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,8 +3,8 @@ import localFont from "next/font/local"
 
 const font = localFont({
   src: [
-    {path: "./styles/fontRegular.woff2", weight: "400"},
-    {path: "./styles/fontBold.woff2", weight: "700"}
+    {path: "./whitelabels/fontRegular.woff2", weight: "400"},
+    {path: "./whitelabels/fontBold.woff2", weight: "700"}
   ]
 })
 
@@ -13,11 +13,11 @@ export const metadata: Metadata = {
   description: "Superintelligence Alignment through Financial Engineering." 
 }
 
-export default function RootLayout({children}: {children: React.ReactNode}) {
+export default function Layout({children}: {children: React.ReactNode}) {
   return (
     <html lang="en" className={`${font.className}`}>
       <head>
-        <link rel="icon" type="image/png" sizes="96x96" href="/styles/iconLight.png" />
+        <link rel="icon" type="image/png" sizes="96x96" href="/whitelabels/iconLight.png" />
       </head>
       <body>
         {children}
