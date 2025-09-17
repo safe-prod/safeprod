a5b6eeec2b6f62f929a694ec7fc208aeea0f9d56
SAFE <safeprod@protonmail.com>
2025-09-17 12:28:08 +0530

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,12 @@
-import { style } from "./style/style.ts" 
 import type { Metadata } from "next"
+import localFont from "next/font/local"
+
+const font = localFont({
+  src: [
+    {path: "./styles/fontRegular.woff2", weight: "400"},
+    {path: "./styles/fontBold.woff2", weight: "700"}
+  ]
+})
 
 export const metadata: Metadata = {
   title: "SAFE",
@@ -8,7 +15,7 @@ export const metadata: Metadata = {
 
 export default function RootLayout({children}: {children: React.ReactNode}) {
   return (
-    <html lang="en" className={`${style.font}`}>
+    <html lang="en" className={`${font.className}`}>
       <head>
         <link rel="icon" type="image/png" sizes="96x96" href="/styles/iconLight.png" />
       </head>
