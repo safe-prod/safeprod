64a14214bb27356f3434e88681ab35017a350683
SAFE <safeprod@protonmail.com>
2025-09-17 20:35:36 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,8 @@
 "use client"
-import { useAuth } from "./contexts/AuthContext"
-
-const { signOut } = useAuth()
 
 export default function Page() {
   return (
     <div>
-      <button onClick={() => signOut()}>Logout</button>
     </div>
   )
 }
