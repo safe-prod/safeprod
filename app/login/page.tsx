ff8d18a1abf111fa5187c2673b5121f6e9d38e2d
SAFE <safeprod@protonmail.com>
2025-09-18 18:19:11 +0530

    Update page.tsx

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,6 +1,6 @@
 "use client"
 import { useState } from "react"
-import { useAuth } from "../contexts/AuthContext"
+import { useAuth } from "../../contexts/AuthContext"
 
 export default function LoginPage() {
   const { signIn, signUp, signOut, user } = useAuth()
