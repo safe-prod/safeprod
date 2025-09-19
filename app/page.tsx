a7375f3c6d10f62ad8f60614e0eaef88f9b02bab
SAFE <safeprod@protonmail.com>
2025-09-19 15:39:28 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,16 @@
 "use client"
+import { useState, useRef } from "react"
+import { usePrice } from "../products/usePrice"
 
 export default function Page() {
+  const realProductRef = useRef(null)
+  const [ realProduct, setRealProduct ] = useState("")
+  const { price, setPrice } = usePrice([])
   return (
     <div>
+      <input ref={realProductRef} type="text" placeholder="Real product" />
+      <button onClick={() => {setRealProduct(realProductRef.current.value)}}>Price</button>
+      <div>{price}</div>
     </div>
   )
 }
