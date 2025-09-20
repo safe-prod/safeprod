6e0a31eeca61d832d578a6c4ca855ef8f6c8d1cf
SAFE <safeprod@protonmail.com>
2025-09-20 16:26:47 +0530

    Update and rename usePrice.ts to price.ts

---
+++ product/price.ts
@@ -0,0 +1,13 @@
+import { embed } from "ai"
+
+export async function price(realProduct: string): number[] {
+  try {
+    const { embedding } = await embed({
+      model: `${process.env.NEXT_PUBLIC_EMBEDDING_MODEL}`,
+      value: realProduct
+    })
+    return embedding
+  } catch (error) {
+    return (error as Error).message || "An unknown error occurred"
+  }
+}
