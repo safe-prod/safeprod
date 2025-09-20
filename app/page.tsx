120f57a541fb67254a6b380e43d3dc8b154500b4
SAFE <safeprod@protonmail.com>
2025-09-20 17:53:15 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
 "use client"
 import Auth from "../product/Auth"
+import Price from "../product/Price"
 
 export default function Page() {
   return (
     <div>
       <Auth />
+      <Price />
     </div>
   )
 }
