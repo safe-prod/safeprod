8ad4e1738a5d565e06eb59d617369bdec2dd246e
SAFE <safeprod@protonmail.com>
2025-09-20 16:29:41 +0530

    Update price.ts

--- product/price.ts
+++ product/price.ts
@@ -1,6 +1,6 @@
 import { embed } from "ai"
 
-export async function getPrice(realProduct: string): Promise<number[]> | Error {
+export async function getPrice(realProduct: string) {
   try {
     const { embedding } = await embed({
       model: `${process.env.NEXT_PUBLIC_EMBEDDING_MODEL}`,
